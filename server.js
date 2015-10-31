57adc77206925466fc15a0615948e9f4ad5437c0
cposada23 <cposadaa@gmail.com>
2015-10-31 13:04:09 -0500

    chat en node.js

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express"),
+http = require("http"),
+app = express(),
+server = http.createServer(app),
+path = require('path'),
+port = 3000;
+ 
+app.get("/", function(req, res){
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+ 
+server.listen(port);
+console.log("Listening on port " + port);
+
+var io = require('socket.io').listen(server);
+io.sockets.on('connection', function (socket) {
+	socket.on('send',function  (data) {
+		io.sockets.emit('message', data);
+	});
+});
